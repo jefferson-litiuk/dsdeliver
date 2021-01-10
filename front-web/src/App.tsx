3fccc4c6a740bcf6b63728695d133bf644889872
Jefferson Litiuk <jefferson.litiuk@hotmail.com>
2021-01-10 18:36:12 -0300

    Finish front-web

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,9 +1,13 @@
 import './App.css';
 import Routes from './Routes';
+import { ToastContainer,  } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-  return (
+  return (<>
     <Routes/>
+    <ToastContainer />
+    </>
   );
 }
 
--- front-web/src/Orders/types.ts
+++ front-web/src/Orders/types.ts
@@ -1,3 +1,5 @@
+import { type } from "os"
+
 export type Product = {
 
     id: number;
@@ -8,7 +10,15 @@ export type Product = {
 }
 
 export type OrderLocationData = {
-    latitude:number;
-    longitude:number;
-    address:string;
+    latitude: number;
+    longitude: number;
+    address: string;
 }
+
+type ProductId = {
+    id: number;
+}
+
+export type OrderPayload = {
+    products: ProductId[];
+} & OrderLocationData;
